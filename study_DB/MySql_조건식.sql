b680f001542196dbcc5ad1ffcf292eb1283e280b
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-07-11 15:22:29 +0900

    Update MySql_조건식.sql

--- study_DB/MySql_조건식.sql
+++ study_DB/MySql_조건식.sql
@@ -41,6 +41,17 @@ where sal between 1600 and 3000;
 
 select *from dept;
 
+-- dept테이블에서 10번 부서의 위치를 'NEW YORK'에서 '서울'로 변경하시오.
+update dept set loc='서울'
+where deptno=10;
+
+-- 사원테이블(emp)에서 30번 부서에 근무하는 사원들의 급여를 10%인상하시오.
+update emp set sal= sal + sal*0.1
+where deptno=30;
+
+select * from emp;
+select *from dept;
+
 -- 커미션을 받는 사원의 사원번호, 사원명, 부서번호, 커미션을 출력하시오.
 select empno, ename, deptno, comm
 from emp
@@ -69,3 +80,15 @@ from emp;
 select empno, ename, sal, sal+ifnull(comm, 0) "연봉"
 from emp;
 #컬럼의 값이 null이였을때  적당한 값으로 대체!!  :  ifnull( null예상컬럼, 대체 데이터)
+
+-- 사원들의 사원번호, 사원명, 커미션을 출력하되 커미션을 받지않는(커미션의 값이 null) 사원들은 0으로 출력하시오.
+select empno, ename, ifnull(comm,0)
+from emp;
+-- 사원들의 사원번호, 사원명, 커미션을 출력하되 커미션을 받지않는(커미션의 값이 null) 사원들은 '없음'으로 출력하시오.
+select empno, ename, ifnull(comm,'없음')
+from emp;
+
+-- 10번 부서에 근무하는 사원들의 사원번호, 사원명, 직책, 매니저(상사)를 출력하시오. (단, 매니저가 없는 경우 'CEO'를 출력하시오.)
+select empno, ename, job , ifnull(mgr,'CEO') mgr
+from emp
+where deptno=10;
